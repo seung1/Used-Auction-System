08c44dbd40b3a0fd647a0ccdde31a7c76c2d8342
이해인 <eehaein2@gmail.com>
2021-05-17 23:57:39 +0900

    mongo connection create

--- Develop/used_auction_server/src/index.js
+++ Develop/used_auction_server/src/index.js
@@ -1,5 +1,7 @@
 // import Koa from 'koa';
 const Koa = require('koa');
+const mongoose = require('mongoose');
+require('dotenv').config();
 // import Router from 'koa-router';
 // import bodyParser from 'koa-bodyparser';
 // import mongoose from 'mongoose';
@@ -7,15 +9,26 @@ const Koa = require('koa');
 // import path from 'path';
 // import send from 'koa-send';
 
+const { PORT, MONGO_URI } = process.env;
 const app = new Koa();
 // const router = new Router();
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 app.use((ctx) => {
   ctx.body = 'auction';
 });
 
-app.listen(4000, () => {
-  console.log('Listening');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
 });
 
 /* eslint-disable no-global-assign */
