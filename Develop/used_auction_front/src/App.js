712911a95fc5391c4312303c9a91242e44e89772
이해인 <eehaein2@gmail.com>
2021-05-17 23:03:17 +0900

    directory route change

---
+++ Develop/used_auction_front/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>USER-Auction-system</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
