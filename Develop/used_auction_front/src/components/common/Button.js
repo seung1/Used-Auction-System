6e9499241316ddc09963d6410fa113177ef3f88d
이해인 <eehaein2@gmail.com>
2021-05-23 01:18:14 +0900

    button 컴포넌트 추가

---
+++ Develop/used_auction_front/src/components/common/Button.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
+
+const buttonStyle = css`
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: -1rem 1rem;
+  color: white;
+  outline: none;
+  cursor: pointer;
+  background: #3e81f6 !important;
+  &:hover {
+    background: black !important;
+  }
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+  ${(props) =>
+    props.cyan &&
+    css`
+      background: #3e81f6;
+      &:hover {
+        background: grey;
+      }
+    `}
+    &:disabled {
+    background: grey;
+    color: grey;
+    cursor: not-allowed;
+  }
+`;
+
+const StyledButton = styled.button`
+  ${buttonStyle}
+`;
+
+const StyledLink = styled(Link)`
+  ${buttonStyle}
+`;
+
+const Button = (props) => {
+  return props.to ? (
+    <StyledLink {...props} cyan={props.cyan ? 1 : 0} />
+  ) : (
+    <StyledButton {...props} />
+  );
+};
+
+export default Button;
