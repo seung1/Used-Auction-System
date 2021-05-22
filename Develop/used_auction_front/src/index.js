9b7c4877a12e9ade5802b1e5347152ec33b6c717
이해인 <eehaein2@gmail.com>
2021-05-23 01:15:05 +0900

    sagaMiddleware 추가 및 composeWithDevTools에 적용

--- Develop/used_auction_front/src/index.js
+++ Develop/used_auction_front/src/index.js
@@ -4,12 +4,27 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer, { rootSaga } from "./modules";
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
