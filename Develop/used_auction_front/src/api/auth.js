0a839122a850a9f4c828bc8921039d6b4fca7c1f
이해인 <eehaein2@gmail.com>
2021-05-23 01:15:42 +0900

    axios api auth 메서드 추가

--- Develop/used_auction_front/src/api/auth.js
+++ Develop/used_auction_front/src/api/auth.js
@@ -3,3 +3,8 @@ import http from "./http";
 // LOGIN
 export const login = ({ username, password }) =>
   http.post("/api/auth/login", { username, password });
+
+export const register = ({ username, password }) =>
+  http.post("/api/auth/register", { username, password });
+
+export const check = () => clientInformation.get("/api/auth/check");
