f6a98f4f1c9baa10c2db6cfe6f96800471c90be7
이해인 <eehaein2@gmail.com>
2021-05-24 22:18:44 +0900

    Register container 생성

---
+++ Develop/used_auction_front/src/containers/auth/RegisterForm.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { changeField, initializeForm, login } from "../../modules/auth";
+import AuthForm from "../../components/auth/AuthForm";
+// import { check } from "../../modules/user";
+
+const RegisterForm = ({ history }) => {
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+    form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
+  }));
+  // 인풋 변경 이벤트 핸들러
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: "register",
+        key: name,
+        value,
+      })
+    );
+  };
+
+  // 폼 등록 이벤트 핸들러
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = form;
+    dispatch(login({ username, password }));
+  };
+
+  // 컴포넌트가 처음 렌더링 될 때 form 을 초기화함
+  useEffect(() => {
+    dispatch(initializeForm("register"));
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.log("오류 발생");
+      console.log(authError);
+      setError("회원가입 실패");
+      return;
+    }
+    if (auth) {
+      console.log("회원가입 성공");
+      //   dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  //   useEffect(() => {
+  //     if (user) {
+  //       history.push("/");
+  //       try {
+  //         localStorage.setItem("user", JSON.stringify(user));
+  //       } catch (e) {
+  //         console.log("localStorage is not working");
+  //       }
+  //     }
+  //   }, [history, user]);
+
+  return (
+    <AuthForm
+      type="register"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      error={error}
+    />
+  );
+};
+
+export default withRouter(RegisterForm);
