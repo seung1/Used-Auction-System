e01fd85a4436335e2a572d2440471d3e5d6d09f1
이해인 <eehaein2@gmail.com>
2021-05-24 22:29:56 +0900

    user module 및 userSaga 생성

--- Develop/used_auction_front/src/modules/index.js
+++ Develop/used_auction_front/src/modules/index.js
@@ -2,14 +2,16 @@ import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 import loading from "./loading";
 import auth, { authSaga } from "./auth";
+import user, { userSage } from "./user";
 
 const rootReducer = combineReducers({
   loading,
   auth,
+  user,
 });
 
 export function* rootSaga() {
-  yield all([authSaga()]);
+  yield all([authSaga(), userSaga()]);
 }
 
 export default rootReducer;
