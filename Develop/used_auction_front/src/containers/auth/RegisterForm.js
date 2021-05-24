07145fe9369695b8266e4c0919b3282c9421009c
이해인 <eehaein2@gmail.com>
2021-05-24 22:32:29 +0900

    registerform에 user Check추가

--- Develop/used_auction_front/src/containers/auth/RegisterForm.js
+++ Develop/used_auction_front/src/containers/auth/RegisterForm.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { changeField, initializeForm, login } from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
-// import { check } from "../../modules/user";
+import { check } from "../../modules/user";
 
 const RegisterForm = ({ history }) => {
   const [error, setError] = useState(null);
@@ -47,20 +47,20 @@ const RegisterForm = ({ history }) => {
     }
     if (auth) {
       console.log("회원가입 성공");
-      //   dispatch(check());
+      dispatch(check());
     }
   }, [auth, authError, dispatch]);
 
-  //   useEffect(() => {
-  //     if (user) {
-  //       history.push("/");
-  //       try {
-  //         localStorage.setItem("user", JSON.stringify(user));
-  //       } catch (e) {
-  //         console.log("localStorage is not working");
-  //       }
-  //     }
-  //   }, [history, user]);
+  useEffect(() => {
+    if (user) {
+      history.push("/");
+      try {
+        localStorage.setItem("user", JSON.stringify(user));
+      } catch (e) {
+        console.log("localStorage is not working");
+      }
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
--- Develop/used_auction_front/src/modules/index.js
+++ Develop/used_auction_front/src/modules/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 import loading from "./loading";
 import auth, { authSaga } from "./auth";
-import user, { userSage } from "./user";
+import user, { userSaga } from "./user";
 
 const rootReducer = combineReducers({
   loading,
