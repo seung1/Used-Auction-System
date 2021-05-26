e30750fadc522ea91bb4c3a9596afd70ae90464a
ICU222 <jason2560@naver.com>
2021-05-26 21:30:57 +0900

    change mypage

--- Develop/used_auction_front/src/pages/LandingPage.js
+++ Develop/used_auction_front/src/pages/LandingPage.js
@@ -3,7 +3,17 @@ import React from "react";
 const LandingPage = () => {
   return (
     <>
-      <div className="App">AUCTION MAIN PAGE</div>
+      <div className="App">AUCTION MYPAGE</div>
+      <div></div>
+      <div></div>
+
+      <div className="App">test</div>
+    
+       <div>
+      <button onClick>뒤로가기</button>
+      <button onClick>홈으로</button>
+    </div>
+  
     </>
   );
 };
