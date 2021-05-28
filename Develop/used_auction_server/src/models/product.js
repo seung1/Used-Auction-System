deec2045693bc1f4558f515b459df8fd3a542e7b
ICU222 <jason2560@naver.com>
2021-05-29 00:26:47 +0900

    trying to find source of the printing db...

--- Develop/used_auction_server/src/models/product.js
+++ Develop/used_auction_server/src/models/product.js
@@ -1,24 +1,37 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-const {Schema} = mongoose;
+// 
 
-// const autoIncrement = require('moongoose-auto-increment');
-// autoIncrement.initialize(mongoose.connection);
-
-
-// use schema from subgroup3
 const ProductSchema = new Schema({
-    product_number : Number,    // 물건 번호
-    start_date : {type : Date, default : Date.now}, // 등록 시작일, 자동 생성
-    product_name : String,  // 물건 이름
-    price : {type : Number, default : 0},
-    user_name: String, // 판매 등록 유저
-    category : String,  // 물건 카테고리
-    local_area : String,    // 판매 지역
-    bidding_id : {type : String, default : ''},    // 현 입찰자 아이디
-    total_bidding : [String],  // 현재까지 입찰자 리스트 -> 인기 매물 위함
-    soldout : {type : Boolean, default : false} // 아직 판매중인지
+
+    product_name : String,  
+    owner_id : String,
+
+    start_date : {type : Date, default : Date.now}, 
+    current_time : {type : Date},
+    end_time : {type : Date},
+
+    start_price : {type : Number, default : 0},
+    current_price : {type : Number, default : 0},
+    
+    bidding_id : {type : String, default : ''},   
+    
 });
+
+
+
+ProductSchema.method.getproduct = function(product_name){
+    return this.findOne({ product_name })
+}
+ProductSchema.method.getsprice = function(start_price){
+    return this.findOne({ start_price })
+}
+ProductSchema.method.getcprice = function(current_price){
+    return this.findOne({ current_price })
+}
+ProductSchema.method.getenddate = function(end_date){
+    return this.findOne({ end_date })
+}
 
 
 const Product = mongoose.model('Product', ProductSchema);
